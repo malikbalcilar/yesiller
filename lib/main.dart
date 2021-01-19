d1025325382a3883ad8b364f2e42dccc2cea5133
malik <balcilarmalik@gmail.com>
2021-01-19 12:10:57 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/// Material Application
+class YesillerWebSite extends StatelessWidget {
+  ///
+  const YesillerWebSite({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

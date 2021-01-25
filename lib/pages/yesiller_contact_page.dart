ae4134b3327ac1696f52c81d34fa33e993a94cd9
Mehmet Yaz <mehmetyaz@github.com>
2021-01-25 03:27:29 +0300

    Flutter upgraded

--- lib/pages/yesiller_contact_page.dart
+++ lib/pages/yesiller_contact_page.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
 import 'dart:html';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart';
 
 import 'package:url_launcher/url_launcher.dart';
 import 'package:yesiller/src/addresses.dart';
@@ -24,16 +27,32 @@ class _YesillerContactPageState extends State<YesillerContactPage> {
             borderSide:
                 BorderSide(width: 0.5, color: Colors.white.withOpacity(1))),
         focusedBorder: OutlineInputBorder(
-            borderSide:
-                BorderSide(width: 1, color: Colors.white)),
-        labelStyle: TextStyle(color: Colors.white,fontSize: 16),
+            borderSide: BorderSide(width: 1, color: Colors.white)),
+        labelStyle: TextStyle(color: Colors.white, fontSize: 16),
         filled: true,
         labelText: label);
   }
 
   List<String> _form = ["İsim", "Soyisim", "Telefon", "E-posta", "Mesaj"];
 
+  List<TextEditingController> editingControllers = [
+    TextEditingController(),
+    TextEditingController(),
+    TextEditingController(),
+    TextEditingController(),
+    TextEditingController(),
+  ];
+
   GoogleMapController controller;
+
+  bool sending = false;
+
+  _showSnack(String mesaj) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(mesaj),
+      duration: Duration(seconds: 5),
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -78,6 +97,8 @@ class _YesillerContactPageState extends State<YesillerContactPage> {
                                   padding:
                                       const EdgeInsets.symmetric(vertical: 10),
                                   child: TextField(
+                                    controller:
+                                        editingControllers[_form.indexOf(e)],
                                     cursorWidth: 1,
                                     cursorColor: const Color(0xFFFFFFFF)
                                         .withOpacity(0.8),
@@ -90,24 +111,124 @@ class _YesillerContactPageState extends State<YesillerContactPage> {
                             .toList(),
                       ),
                     ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 15, top: 35),
+                          child: Container(
+                            height: 37,
+                            width: 110,
+                            child: RaisedButton(
+                              textColor: Color(0xFF042a1d),
+                              onPressed: () async {
+                                ///
+                                var emailLocalValid = editingControllers[3]
+                                            .text !=
+                                        null &&
+                                    (RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                        .hasMatch(editingControllers[3].text));
 
-                    Container(
-                      child: Row(mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(right: 15,top:35),
-                            child: Container(height: 37,
-                              width: 110,
-                              child: RaisedButton(textColor: Color(0xFF042a1d),
-                                onPressed: (){},
-                                child: Text("Gönder",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w900
-                                ),),
-                                color: Color(0xFFfbd5e2),
-                              ),
+                                if (!emailLocalValid) {
+                                  _showSnack(
+                                      "Lütfen Geçerli Bir Mail Adresi Girin");
+                                  return;
+                                }
+
+                                ///
+                                var nameValid = editingControllers[0].text !=
+                                        null &&
+                                    RegExp(r'^[a-zA-Z ğüşöçİĞÜŞÖÇ]+$')
+                                        .hasMatch(editingControllers[0].text);
+
+                                if (!nameValid) {
+                                  _showSnack("Lütfen Geçerli Bir İsim Girin");
+                                  return;
+                                }
+
+                                ///
+                                var lastNameValid = editingControllers[1]
+                                            .text !=
+                                        null &&
+                                    RegExp(r'^[a-zA-Z ğüşöçİĞÜŞÖÇ]+$')
+                                        .hasMatch(editingControllers[1].text);
+
+                                if (!lastNameValid) {
+                                  _showSnack(
+                                      "Lütfen Geçerli Bir Soyisim Girin");
+                                  return;
+                                }
+
+                                var v = editingControllers[2].text;
+
+                                var phoneValid = v != null &&
+                                    v.isNotEmpty &&
+                                    (v[0] == "0" ||
+                                        v[0] == "9" ||
+                                        v[0] == "5" ||
+                                        v[0] == "+") &&
+                                    ((v[0] == "0" && v.length == 11) ||
+                                        (v[0] == "5" && v.length == 10) ||
+                                        (v[0] == "+" && v.length == 13) ||
+                                        (v[0] == "9" && v.length == 12));
+
+                                if (!phoneValid) {
+                                  _showSnack(
+                                      "Lütfen Geçerli Bir Telefon Numarası Girin");
+                                  return;
+                                }
+
+                                var messageValid =
+                                    editingControllers[4].text != null &&
+                                        editingControllers[4].text.isNotEmpty;
+
+                                if (!messageValid) {
+                                  _showSnack("Lütfen Geçerli Bir Mesaj Girin");
+                                  return;
+                                }
+
+                                setState(() {
+                                  sending = true;
+                                });
+
+                                await post(
+                                    "https://us-central1-yesiller.cloudfunctions.net/sendMail",
+                                    headers: {
+                                      'Accept': 'application/json',
+                                      'Content-Type': 'application/json'
+                                    },
+                                    body: json.encode({
+                                      "name": editingControllers[0].text,
+                                      "secondName": editingControllers[1].text,
+                                      "mail": editingControllers[3].text,
+                                      "telefon": editingControllers[2].text,
+                                      "mesaj": editingControllers[4].text
+                                    }));
+
+                                for (var co in editingControllers) {
+                                  co.clear();
+                                }
+
+                                setState(() {
+                                  sending = false;
+                                });
+                              },
+                              child: sending
+                                  ? Container(
+                                      height: 20,
+                                      width: 20,
+                                      child: const CircularProgressIndicator())
+                                  : const Text(
+                                      "Gönder",
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w900),
+                                    ),
+                              color: Color(0xFFfbd5e2),
                             ),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     )
                   ],
                 ),
